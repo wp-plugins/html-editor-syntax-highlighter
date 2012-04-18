07e05ed0a756c619a924bc6b284be8ec0a743a4c
nixdns <nixdns@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-18 11:13:06 +0000

    Plugin files
    git-svn-id: https://plugins.svn.wordpress.org/html-editor-syntax-highlighter/trunk@532778 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ html-editor-syntax-highlighter.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Plugin Name: HTML Editor Syntax Highlighter
+ * Plugin URI: http://
+ * Description: Syntax Highlighting in WordPress HTML Editor
+ * Author: Peter Mukhortov
+ * Author URI: http://mukhortov.com/
+ * Version: 1.0
+ * Requires at least: 3.3
+ * Tested up to: 3.3
+ * Stable tag: 1.0
+ **/
+
+if(preg_match('#' . basename(__FILE__) . '#', $_SERVER['PHP_SELF'])) { die('You are not allowed to call this page directly.'); }
+
+define('HESH_LIBS',plugins_url('/lib/',__FILE__));
+
+class wp_html_editor_syntax {
+	public function __construct(){
+		add_action('admin_init',array(&$this,'admin_init'));
+		add_action('admin_head',array(&$this,'admin_head'));
+		add_action('admin_footer',array(&$this,'admin_footer'));
+	}
+	public function admin_footer(){
+		if (!$this->is_editor())
+			return;
+		?>
+
+		<script type="text/javascript">
+		
+			function runEditorHighlighter(el) {
+				fullscreen.switchmode('html');
+				switchEditors.switchto(document.getElementById("content-html"));
+
+				var editor = CodeMirror.fromTextArea(document.getElementById(el), {
+					mode: "text/html",
+					tabMode: "indent",
+					lineNumbers: true,
+					matchBrackets: true,
+					indentUnit: 4,
+					indentWithTabs: true,
+					enterMode: "keep",
+					lineWrapping: true,
+					onCursorActivity: function() {
+						editor.setLineClass(hlLine, null, null);
+						hlLine = editor.setLineClass(editor.getCursor().line, null, "activeline");
+					},
+					onChange: function(){
+						editor.save();
+					}
+				});
+				var hlLine = editor.setLineClass(0, "activeline");
+				
+				document.getElementById("content-tmce").onclick = function(e){
+					editor.toTextArea();
+					switchEditors.switchto(document.getElementById("content-tmce"));
+					document.getElementById("content-html").onclick = function(e){
+						runEditorHighlighter("content");
+					}
+				}
+				
+				document.getElementById("qt_content_fullscreen").onclick = function(e){
+					editor.toTextArea();
+					fullscreen.switchmode('html');
+					setTimeout('runEditorHighlighter("wp_mce_fullscreen")', 2000);
+					document.getElementById("wp-fullscreen-close").onclick = function(e){
+						fullscreen.off();
+						runEditorHighlighter("content");
+						return false;
+					}
+				}
+			}
+
+			window.onload = function() {
+				runEditorHighlighter("content");
+			}
+
+
+		</script>
+
+		<?php
+	}
+	public function admin_init(){
+		wp_enqueue_script('jquery');	// For AJAX code submissions
+		wp_enqueue_script('jquery-ui-core');
+		wp_enqueue_script('jquery-ui-widget');
+		wp_enqueue_script('jquery-ui-mouse');
+		wp_enqueue_script('jquery-ui-resizable');
+	}
+	public function admin_head(){
+		if (!$this->is_editor())
+			return;
+
+		?>
+				<link rel="stylesheet" href="<?php echo HESH_LIBS; ?>codemirror.css">
+				<script src="<?php echo HESH_LIBS; ?>codemirror.js"></script>
+				<script src="<?php echo HESH_LIBS; ?>xml.js"></script>
+				<script src="<?php echo HESH_LIBS; ?>javascript.js"></script>
+				<script src="<?php echo HESH_LIBS; ?>css.js"></script>
+				<script src="<?php echo HESH_LIBS; ?>htmlmixed.js"></script>
+		<?php
+
+	}
+	
+	private function is_editor(){
+		if (!strstr($_SERVER['SCRIPT_NAME'],'post.php'))
+			return false;
+		return true;
+	}
+}
+
+if (is_admin())
+	$hesh = new wp_html_editor_syntax();
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,19 @@
+=== HTML Editor Syntax Highlighter ===
+Author: Peter Mukhortov
+Tags: html editor, syntax highlighter
+Requires at least: 3.3
+Tested up to: 3.3
+Stable tag: 1.0
+
+Add syntax highlighting to the HTML editor.
+
+== Description ==
+
+
+== Installation ==
+
+1. Upload the 'html-editor-syntax-highlighter' directory to the '/wp-content/plugins/' directory
+2. Activate the plugin on the 'Plugins' page
+
+= 1.0 =
+Initial release.

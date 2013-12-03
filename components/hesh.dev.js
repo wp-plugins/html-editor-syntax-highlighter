66daa54b1e969dd7c493966f10c6a4a50dee4bb0
nixdns <nixdns@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-03 21:40:51 +0000

    Adopted Dev version to use CodeKit
    git-svn-id: https://plugins.svn.wordpress.org/html-editor-syntax-highlighter/trunk@814639 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- components/hesh.dev.js
+++ components/hesh.dev.js
@@ -1,13 +1,33 @@
+// Codemirror v3.20 CSS files:
+// codemirror.js, css.js, htmlmixed.js, javascript.js, xml.js, active-line.js, matchbrackets.js
+
+// @codekit-prepend "codemirror/codemirror.js";
+// @codekit-prepend "codemirror/css.js";
+// @codekit-prepend "codemirror/htmlmixed.js";
+// @codekit-prepend "codemirror/javascript.js";
+// @codekit-prepend "codemirror/xml.js";
+// @codekit-prepend "codemirror/active-line.js";
+// @codekit-prepend "codemirror/matchbrackets.js";
+
+// Concatenate and minify all the files above with the rest of code and save as hesh.min.js
+
+/*
+ * Plugin Name: HTML Editor Syntax Highlighter
+ * Author: Petr Mukhortov
+ * Author URI: http://mukhortov.com/
+ * Version: 1.4.4
+*/
+
 function heshPlugin() {
 	var editor = null,
 		isOn = 0,
 		buttonsAdded = 0,
 		target = document.getElementById("content"),
-		postID = document.getElementById("post_ID") != null ? document.getElementById("post_ID").value : 0,
+		postID = document.getElementById("post_ID") !== null ? document.getElementById("post_ID").value : 0,
 		tab_html = document.getElementById("content-html"),
 		tab_tmce = document.getElementById("content-tmce"),
-		visualEditor = document.cookie.indexOf("editor%3Dtinymce") != -1 ? true : false,
-		visualEditorEnabled = document.getElementById("content-tmce") != null ? true : false,
+		visualEditor = document.cookie.indexOf("editor%3Dtinymce") !== -1 ? true : false,
+		visualEditorEnabled = document.getElementById("content-tmce") !== null ? true : false,
 		fullscreenBox = document.getElementById("wp-content-editor-container"),
 		fullscreenClass = "heshFullscreen",
 		publishButton = document.getElementById('publish'),
@@ -22,6 +42,7 @@ function heshPlugin() {
 			lineWrapping: true,
 			autofocus: true,
 			styleActiveLine: true,
+			electricChars: false,
 			extraKeys: {
 				"F11": function() {
 					toggleFullscreen();
@@ -41,15 +62,15 @@ function heshPlugin() {
 		var toolbar = document.getElementById("ed_toolbar");
 		if (!buttonsAdded) {
 			var toolbarVars = {
-				more: 		['<!--more-->',''],
+				more:		['<!--more-->',''],
 				comment:	['<!-- ',' -->'],
-				code: 		['<code>','</code>'],
-				li: 		['<li>','</li>'],
-				ol: 		['<ol>','</ol>'],
-				ul: 		['<ul>','</ul>'],
-				img: 		['<img src="$" alt="','">','Enter the URL of the image'],
-				ins: 		['<ins>','</ins>'],
-				del: 		['<del>','</del>'],
+				code:		['<code>','</code>'],
+				li:			['<li>','</li>'],
+				ol:			['<ol>','</ol>'],
+				ul:			['<ul>','</ul>'],
+				img:		['<img src="$" alt="','">','Enter the URL of the image'],
+				ins:		['<ins>','</ins>'],
+				del:		['<del>','</del>'],
 				link:		['<a href="$">','</a>','Enter the destination URL'],
 				blockquote:	['\r<blockquote>','</blockquote>\r'],
 				h3:			['<h3>','</h3>'],
@@ -58,7 +79,7 @@ function heshPlugin() {
 				i:			['<em>','</em>'],
 				b:			['<strong>','</strong>']
 			};
-			for (key in toolbarVars) {
+			for (var key in toolbarVars) {
 				var t = toolbarVars[key];
 				var t3 = t[2] ? 'data-prompt="'+t[2]+'"' : '';
 				toolbar.insertAdjacentHTML('afterbegin', '<input type="button" id="cm_content_'+key+'" data-start=\''+t[0]+'\' data-end=\''+t[1]+'\' '+t3+' class="ed_button cm_ed_button" value="'+key+'">');
@@ -72,7 +93,7 @@ function heshPlugin() {
 		var selStart = editor.getCursor("start"),
 			start = this.getAttribute('data-start'),
 			end = this.getAttribute('data-end'),
-			cmPrompt = this.getAttribute('data-prompt'),
+			cmPrompt = this.getAttribute('data-prompt') || null,
 			selText = editor.getSelection();
 		if (cmPrompt) start = start.replace('$',prompt(cmPrompt, ''));
 		editor.replaceSelection(start+selText+end);
@@ -93,7 +114,7 @@ function heshPlugin() {
 		});
 		//Restoring cursor state
 		var curPos = window.name.split(',');
-		if (postID == curPos[0]) {
+		if (postID === curPos[0]) {
 			editor.setCursor(parseFloat(curPos[1]),parseFloat(curPos[2]));
 		}
 		addButtons();
@@ -108,14 +129,14 @@ function heshPlugin() {
 			switchEditors.switchto(this);
 			isOn = 0;
 		}
-	}
+	},
 	toHTML = function() {
 		if (!isOn) {
 			switchEditors.switchto(this);
 			runEditor(target);
 			tab_tmce.onclick = toVisual;
 		}
-	}
+	},
 	resizeEditor = function() {
 		var target = document.querySelector('.CodeMirror'),
 			handle = document.createElement('div'),
@@ -126,20 +147,20 @@ function heshPlugin() {
 				var height = (e.pageY || e.clientY + document.body.scrollTop + document.documentElement.scrollTop) - offsetTop;
 				target.style.height = (height > 10 ? height : 10) + 'px';
 				window.getSelection().removeAllRanges(); //disable selection on resize
-			}
+			};
 		handle.className = 'content-resize-handle';
 		hwrap.appendChild(handle);
 		handle.onmousedown = function() {
 			document.onmousemove = move;
-		}
-		document.onmouseup = function() { 
+		};
+		document.onmouseup = function() {
 			document.onmousemove = null;
 			//editor.focus();
-		}
+		};
 	},
 	toggleFullscreen = function() {
-		fullscreenBox.className = fullscreenBox.className.indexOf(fullscreenClass) == -1 ? fullscreenBox.className +' '+ fullscreenClass : fullscreenBox.className.replace(fullscreenClass, '');
-		this.value = this.value == 'fullscreen' ? 'exit fullscreen' : 'fullscreen';
+		fullscreenBox.className = fullscreenBox.className.indexOf(fullscreenClass) === -1 ? fullscreenBox.className +' '+ fullscreenClass : fullscreenBox.className.replace(fullscreenClass, '');
+		this.value = this.value === 'fullscreen' ? 'exit fullscreen' : 'fullscreen';
 		editor.focus();
 	},
 	fullscreen = function() {
@@ -148,18 +169,18 @@ function heshPlugin() {
 	},
 	addMedia = function() {
 		// Dirty
-		document.getElementById('insert-media-button').onclick = function(){
+		document.getElementById('insert-media-button').onclick = function() {
 			target.value = '';
 			target.className = 'IEtextarea';
 			setTimeout(function(){
-				document.querySelector('.media-button-insert').onclick = function(){
+				document.querySelector('.media-button-insert').onclick = function() {
 					setTimeout(function(){
 						editor.replaceSelection(target.value);
 						editor.save();
 					},400);
-				}
+				};
 			},400);
-		}
+		};
 	};
 
 	/* Initialise */
@@ -170,5 +191,5 @@ function heshPlugin() {
 		if (visualEditorEnabled) tab_tmce.onclick = toVisual;
 	}
 
-};
+}
 window.onload = heshPlugin;
